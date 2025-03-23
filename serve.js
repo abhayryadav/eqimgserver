feccefcf3c73efb3e5d281fac2e9ff19433b921b
Abhay Raj Yadav <abhayryadav0007@gmail.com>
2025-03-23 16:11:29 +0530

    1up

---
+++ serve.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+const port = 3004;
+const photosFolder = path.join(__dirname, 'photos');
+
+app.get('/api/photos', (req, res) => {
+    fs.readdir(photosFolder, (err, files) => {
+        if (err) return res.status(500).json({ error: "Error reading directory" });
+
+        const photoData = files.map(file => {
+            const filePath = path.join(photosFolder, file);
+            const imageData = fs.readFileSync(filePath).toString('base64');
+
+            return `data:image/${path.extname(file).substring(1)};base64,${imageData}`;
+        });
+
+        res.json(photoData);
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
